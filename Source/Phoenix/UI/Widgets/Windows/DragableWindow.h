53d7c234bd15d574409fa9e0d896267665b82b64
King2481 <bjcrum94@gmail.com>
2023-12-17 14:13:31 -0600

    Fixed a bug where the right variable wasnt being used for window drag offset placement.

--- Source/Phoenix/UI/Widgets/Windows/DragableWindow.h
+++ Source/Phoenix/UI/Widgets/Windows/DragableWindow.h
@@ -25,6 +25,7 @@ protected:
 	virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
 	virtual void NativeOnDragDetected(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent, UDragDropOperation*& OutOperation) override;
 
+	UPROPERTY(BlueprintReadOnly, Category = "Dragable Window")
 	FVector2D DragOffset;
 
 	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Dragable Window", meta = (BindWidget))
